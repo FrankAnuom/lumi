2e4955eccb2068bda2d7972b85a618c9398c1a18
Anuom Chika Frank <anuomochika@gmail.com>
2025-08-07 03:50:51 +0100

    Update App.jsx

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Signup from "./pages/SignUp";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
 
